b5cbe830e43715b64cb9968e25239609ed7028aa
Michal Harish <michal.harish@gmail.com>
2017-09-30 13:37:36 +0100

    IllegalStateException default handler

--- core/src/main/scala/io/amient/affinity/core/actor/GatewayHttp.scala
+++ core/src/main/scala/io/amient/affinity/core/actor/GatewayHttp.scala
@@ -171,6 +171,7 @@ trait GatewayHttp extends ServicesApi {
     case e: ExecutionException  => handleException(e.getCause)
     case e: NoSuchElementException => errorResponse(e, NotFound, if (e.getMessage == null) "" else e.getMessage, headers)
     case e: IllegalArgumentException => errorResponse(e, BadRequest, if (e.getMessage == null) "" else e.getMessage, headers)
+    case e: IllegalStateException => errorResponse(e, Conflict, if (e.getMessage == null) "" else e.getMessage, headers)
     case e: java.lang.AssertionError => errorResponse(e, BadRequest, if (e.getMessage == null) "" else e.getMessage, headers)
     case e: scala.NotImplementedError => errorResponse(e, NotImplemented, if (e.getMessage == null) "" else e.getMessage, headers)
     case e: UnsupportedOperationException =>errorResponse(e, NotImplemented, if (e.getMessage == null) "" else e.getMessage, headers)
