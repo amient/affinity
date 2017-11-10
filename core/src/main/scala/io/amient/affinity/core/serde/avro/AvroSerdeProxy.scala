86b9a0a4ac3d877824d2f16427a846e3c2bef031
michal-harish <michal.harish@gmail.com>
2017-11-10 15:05:18 +0000

    #66 avro serde proxy may not be configured so using Try

--- core/src/main/scala/io/amient/affinity/core/serde/avro/AvroSerdeProxy.scala
+++ core/src/main/scala/io/amient/affinity/core/serde/avro/AvroSerdeProxy.scala
@@ -5,19 +5,21 @@ import com.typesafe.config.Config
 import io.amient.affinity.avro.AvroSerde
 import io.amient.affinity.core.serde.{Serde, Serdes}
 
+import scala.util.Try
+
 final class AvroSerdeProxy(tools: Serdes) extends Serde[Any] {
 
   def this(config: Config) = this(Serde.tools(config))
 
   def this(system: ExtendedActorSystem) = this(system.settings.config)
 
-  val internal: AvroSerde = AvroSerde.create(tools.config)
+  val internal = Try(AvroSerde.create(tools.config))
 
-  override def fromBytes(bytes: Array[Byte]): Any = internal.fromBytes(bytes)
+  override def fromBytes(bytes: Array[Byte]): Any = internal.get.fromBytes(bytes)
 
-  override def toBytes(obj: Any): Array[Byte] = internal.toBytes(obj)
+  override def toBytes(obj: Any): Array[Byte] = internal.get.toBytes(obj)
 
-  override def close(): Unit = if (internal != null) internal.close()
+  override def close(): Unit = if (internal != null) internal.get.close()
 
   override def identifier: Int = 200
 
