b1c567de447b7ddc71ca4c4fef527d76fcb95951
michal-harish <michal.harish@gmail.com>
2017-11-13 20:18:57 +0000

    #32 schema evolution compatibility test for cf registry

--- avro/src/test/scala/io/amient/affinity/avro/CfAvroSchemaRegistrySpec.scala
+++ avro/src/test/scala/io/amient/affinity/avro/CfAvroSchemaRegistrySpec.scala
@@ -1,12 +1,39 @@
 package io.amient.affinity.avro
 
+import com.typesafe.config.{ConfigFactory, ConfigValueFactory}
+import io.amient.affinity.avro.schema.CfAvroSchemaRegistry
 import io.amient.affinity.kafka.EmbeddedCfRegistry
+import org.apache.avro.Schema
 import org.scalatest.{FlatSpec, Matchers}
 
 class CfAvroSchemaRegistrySpec extends FlatSpec with Matchers with EmbeddedCfRegistry {
 
   override def numPartitions = 1
 
-  //TODO port existing core/CfSchemaRegistrySpec to this module
-  //TODO #32 "CfAvroSchemaRegistry" should "reject incompatible schema for an existing type"
+  behavior of "CfAvroSchemaRegistry"
+
+  it should "reject incompatible schema registration" in {
+
+    val serde = new CfAvroSchemaRegistry(ConfigFactory.defaultReference.withValue(
+      CfAvroSchemaRegistry.CONFIG_CF_REGISTRY_URL_BASE, ConfigValueFactory.fromAnyRef(registryUrl)
+    ))
+    serde.register(classOf[SimpleKey])
+    serde.register(classOf[SimpleRecord])
+    serde.initialize()
+
+    val v1schema = new Schema.Parser().parse("{\"type\":\"record\",\"name\":\"Record\",\"namespace\":\"io.amient.affinity.avro\",\"fields\":[{\"name\":\"items\",\"type\":{\"type\":\"array\",\"items\":{\"type\":\"record\",\"name\":\"SimpleRecord\",\"fields\":[{\"name\":\"id\",\"type\":{\"type\":\"record\",\"name\":\"SimpleKey\",\"fields\":[{\"name\":\"id\",\"type\":\"int\"}]},\"default\":{\"id\":0}},{\"name\":\"side\",\"type\":{\"type\":\"enum\",\"name\":\"SimpleEnum\",\"symbols\":[\"A\",\"B\",\"C\"]},\"default\":\"A\"},{\"name\":\"seq\",\"type\":{\"type\":\"array\",\"items\":\"SimpleKey\"},\"default\":[]}]}},\"default\":[]},{\"name\":\"removed\",\"type\":\"int\",\"default\":0}]}")
+    serde.register(classOf[Record], v1schema)
+
+    serde.register(classOf[Record])
+    serde.initialize()
+
+    val v3schema = new Schema.Parser().parse("{\"type\":\"record\",\"name\":\"Record\",\"namespace\":\"io.amient.affinity.avro\",\"fields\":[{\"name\":\"data\",\"type\":\"string\"}]}")
+    serde.register(classOf[Record], v3schema)
+    val thrown = intercept[RuntimeException] {
+      serde.initialize()
+    }
+    thrown.getMessage should startWith("Schema being registered is incompatible with the latest schema ")
+
+
+  }
 }
