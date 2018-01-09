62918da2fdd7f2337090e356fe5a61e48e262ab4
Michal Harish <michal.harish@gmail.com>
2018-01-09 11:06:49 +0000

    #75 initial idea for more future-proof partitioning strategy

---
+++ kafka/kafka_0.10/storage-kafka/src/test/scala/io/amient/affinity/kafka/KafkaPartitionerSpec.scala
@@ -0,0 +1,33 @@
+package io.amient.affinity.kafka
+
+import java.util
+import java.util.{Collection, Set}
+
+import io.amient.affinity.core.Murmur2Partitioner
+import org.apache.kafka.common.{Cluster, Node, PartitionInfo}
+import org.scalatest.{FlatSpec, Matchers}
+
+class KafkaPartitionerSpec extends FlatSpec with Matchers {
+
+  behavior of "kafka.DefaultPartitioner"
+
+  it should "have identical method to Murmur2Partitioner" in {
+    val kafkaPartitioner = new org.apache.kafka.clients.producer.internals.DefaultPartitioner()
+    val affinityPartitioner = new Murmur2Partitioner
+    val value = "test-value-for-partitioner"
+    val bytes: Array[Byte] = value.getBytes
+    val kafkaP = kafkaPartitioner.partition("test", value, bytes, value, bytes, new Cluster("mock-cluster",
+      util.Arrays.asList[Node](),
+      util.Arrays.asList(
+        new PartitionInfo("test", 0, null, Array(), Array()),
+        new PartitionInfo("test", 1, null, Array(), Array()),
+        new PartitionInfo("test", 2, null, Array(), Array()),
+        new PartitionInfo("test", 3, null, Array(), Array())
+      ),
+      new util.HashSet[String],
+      new util.HashSet[String]))
+    val affinityP = affinityPartitioner.partition(bytes, 4)
+    kafkaP should equal(affinityP)
+  }
+
+}
