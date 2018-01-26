6b976d1afbbe21b1575b957b59a65cf4c23f73e3
Michal Harish <michal.harish@gmail.com>
2018-01-26 10:00:01 +0000

    increasing timeout on specs offending when cpu is busy

--- core/src/test/scala/io/amient/affinity/core/storage/StateSpec.scala
+++ core/src/test/scala/io/amient/affinity/core/storage/StateSpec.scala
@@ -23,6 +23,8 @@ class StateSpec extends TestKit(ActorSystem.create("test",
   )).withFallback(ConfigFactory.defaultReference)))
   with ImplicitSender with FlatSpecLike with Matchers with BeforeAndAfterAll {
 
+  val specTimeout = 5 seconds
+
   override def afterAll {
     TestKit.shutdownActorSystem(system)
   }
@@ -38,8 +40,8 @@ class StateSpec extends TestKit(ActorSystem.create("test",
       State.StateConf.External.path -> "true"
     )))
     val state = State.create[Long, ExpirableValue]("read-only-store", 0, stateConf, 1, system)
-    an[RuntimeException] should be thrownBy (Await.result(state.insert(1L, ExpirableValue("one", 1)), 2 seconds))
-    an[RuntimeException] should be thrownBy (Await.result(state.delete(1L), 2 seconds))
+    an[RuntimeException] should be thrownBy (Await.result(state.insert(1L, ExpirableValue("one", 1)), specTimeout))
+    an[RuntimeException] should be thrownBy (Await.result(state.delete(1L), specTimeout))
   }
 
   it should "work without ttl" in {
@@ -55,7 +57,7 @@ class StateSpec extends TestKit(ActorSystem.create("test",
     Await.result(Future.sequence(List(
       state.insert(1L, ExpirableValue("one", nowMs - 9000)),
       state.insert(2L, ExpirableValue("two", nowMs - 3000)),
-      state.insert(3L, ExpirableValue("three", nowMs)))), 2 seconds)
+      state.insert(3L, ExpirableValue("three", nowMs)))), specTimeout)
     state(1L) should be(Some(ExpirableValue("one", nowMs - 9000)))
     state(2L) should be(Some(ExpirableValue("two", nowMs - 3000)))
     state(3L) should be(Some(ExpirableValue("three", nowMs)))
@@ -76,7 +78,7 @@ class StateSpec extends TestKit(ActorSystem.create("test",
     Await.result(Future.sequence(List(
       state.insert(1L, ExpirableValue("one", nowMs - 9000)),
       state.insert(2L, ExpirableValue("two", nowMs - 3000)),
-      state.insert(3L, ExpirableValue("three", nowMs)))), 2 seconds)
+      state.insert(3L, ExpirableValue("three", nowMs)))), specTimeout)
     state(1L) should be(None)
     state(2L) should be(Some(ExpirableValue("two", nowMs - 3000)))
     state(3L) should be(Some(ExpirableValue("three", nowMs)))
