800329a4aef017a8343c664c23895c6a80da9c4f
Michal Harish <michal.harish@gmail.com>
2018-02-14 18:12:36 +0000

    fixed wrong package in CompactRDD

--- spark/src/main/scala/io/amient/affinity/spark/CompactRDD.scala
+++ spark/src/main/scala/io/amient/affinity/spark/CompactRDD.scala
@@ -17,11 +17,10 @@
  * limitations under the License.
  */
 
-package io.amient.util.spark
+package io.amient.affinity.spark
 
 import io.amient.affinity.core.serde.AbstractSerde
 import io.amient.affinity.core.util.{EventTime, TimeRange}
-import io.amient.affinity.spark.BinaryCompactRDD
 import io.amient.affinity.stream._
 import org.apache.spark.rdd.RDD
 import org.apache.spark.util.LongAccumulator
