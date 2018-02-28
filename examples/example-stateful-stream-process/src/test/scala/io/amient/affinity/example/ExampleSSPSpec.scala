291e5d8738ea7877648f1e61c1673507932de71e
michal-harish <michal.harish@gmail.com>
2018-02-28 00:44:03 +0000

    161-example-and-test

--- examples/example-stateful-stream-process/src/test/scala/io/amient/affinity/example/ExampleSSPSpec.scala
+++ examples/example-stateful-stream-process/src/test/scala/io/amient/affinity/example/ExampleSSPSpec.scala
@@ -19,11 +19,80 @@
 
 package io.amient.affinity.example
 
+import java.util.{Properties, UUID}
+
+import com.typesafe.config.{ConfigFactory, ConfigRenderOptions, ConfigValueFactory}
+import io.amient.affinity.avro.MemorySchemaRegistry
+import io.amient.affinity.core.cluster.Node
 import io.amient.affinity.core.util.AffinityTestBase
+import org.scalatest.BeforeAndAfterAll
 import io.amient.affinity.kafka.EmbeddedKafka
+import org.apache.kafka.clients.consumer.ConsumerConfig
+import org.apache.kafka.clients.producer.{KafkaProducer, ProducerConfig}
+import org.apache.kafka.common.serialization.{LongDeserializer, LongSerializer, StringDeserializer, StringSerializer}
 import org.scalatest.{FlatSpec, Matchers}
 
-class ExampleSSPSpec extends FlatSpec with AffinityTestBase with EmbeddedKafka with Matchers {
+import scala.collection.JavaConverters._
+
+class ExampleSSPSpec extends FlatSpec with AffinityTestBase with EmbeddedKafka with BeforeAndAfterAll with Matchers {
   override def numPartitions = 1
-  //TODO
+
+  val MemorySchemaRegistryId = "123423542"
+
+  val config = ConfigFactory.parseMap(Map(
+    "kafka.bootstrap.servers" -> kafkaBootstrap,
+    "affinity.avro.schema.registry.class" -> classOf[MemorySchemaRegistry].getName,
+    "affinity.avro.schema.registry.id" -> MemorySchemaRegistryId,
+    "affinity.node.gateway.http.host" -> "0.0.0.0",
+    "affinity.node.gateway.http.port" -> "0"
+  ).asJava)
+    .withFallback(ConfigFactory.parseResources("example-ssp.conf"))
+    .resolve()
+    .withFallback(ConfigFactory.defaultReference())
+
+
+
+  val node = new Node(config)
+
+  val inputTopic = config.getString("affinity.node.gateway.stream.input-stream.kafka.topic")
+
+  val outputTopic = config.getString("affinity.node.gateway.stream.output-stream.kafka.topic")
+
+  val producer = new KafkaProducer[String, Long](new Properties() {
+    import ProducerConfig._
+    put(BOOTSTRAP_SERVERS_CONFIG, kafkaBootstrap)
+    put(ACKS_CONFIG, "1")
+    put(KEY_SERIALIZER_CLASS_CONFIG, classOf[StringSerializer])
+    put(VALUE_SERIALIZER_CLASS_CONFIG, classOf[LongSerializer])
+    put(CLIENT_ID_CONFIG, UUID.randomUUID.toString)
+  })
+
+  val consumerConfig = new Properties() {
+    import ConsumerConfig._
+    put(BOOTSTRAP_SERVERS_CONFIG, kafkaBootstrap)
+    put(KEY_DESERIALIZER_CLASS_CONFIG, classOf[StringDeserializer].getName)
+    put(VALUE_DESERIALIZER_CLASS_CONFIG, classOf[LongDeserializer].getName)
+    put(GROUP_ID_CONFIG, UUID.randomUUID.toString)
+    put(AUTO_OFFSET_RESET_CONFIG, "earliest")
+    put("schema.registry.class", classOf[MemorySchemaRegistry].getName)
+    put("schema.registry.id", MemorySchemaRegistryId)
+  }
+
+
+  override def beforeAll(): Unit = {
+    node.start()
+    node.awaitClusterReady()
+    println("UP!")
+  }
+
+  override def afterAll(): Unit = {
+    node.shutdown()
+    producer.close()
+    println("DOWN!")
+    Thread.sleep(5000)
+  }
+
+  "ExampleSSP" should "work" in {
+
+  }
 }
