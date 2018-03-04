1a93e016be1e9226bb50e5fd0e3790ec0947e0c5
Michal Harish <michal.harish@gmail.com>
2018-03-04 15:55:34 +0000

    fixed timeout in master transition spec 2

--- core/src/test/scala/io/amient/affinity/core/cluster/MasterTransitionSpec2.scala
+++ core/src/test/scala/io/amient/affinity/core/cluster/MasterTransitionSpec2.scala
@@ -45,7 +45,7 @@ import scala.util.Random
 
 class MasterTransitionSpec2 extends FlatSpec with AffinityTestBase with EmbeddedKafka with Matchers {
 
-  val specTimeout = 15 seconds
+  val specTimeout = 30 seconds
 
   override def numPartitions = 2
 
