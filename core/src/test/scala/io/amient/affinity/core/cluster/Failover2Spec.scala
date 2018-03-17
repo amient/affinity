3e99cf8b7bf902d571c546116f5ffa23e38e01b2
Michal Harish <michal.harish@gmail.com>
2018-03-17 13:07:30 +0000

    #177 Failover2Spec fix 2

--- core/src/test/scala/io/amient/affinity/core/cluster/Failover2Spec.scala
+++ core/src/test/scala/io/amient/affinity/core/cluster/Failover2Spec.scala
@@ -23,8 +23,8 @@ import java.util.concurrent.ConcurrentHashMap
 import java.util.concurrent.atomic.AtomicLong
 
 import akka.http.scaladsl.model.HttpMethods._
-import akka.http.scaladsl.model.StatusCodes._
-import akka.http.scaladsl.model._
+import akka.http.scaladsl.model.{HttpResponse, StatusCode, Uri, headers}
+import akka.http.scaladsl.model.StatusCodes.{OK, SeeOther}
 import akka.util.Timeout
 import com.typesafe.config.ConfigValueFactory
 import io.amient.affinity.Conf
@@ -42,7 +42,7 @@ import scala.collection.JavaConverters._
 import scala.concurrent.duration._
 import scala.concurrent.{Await, Future}
 import scala.language.postfixOps
-import scala.util.Random
+import scala.util.{Failure, Random, Success, Try}
 
 
 class Failover2Spec extends FlatSpec with AffinityTestBase with EmbeddedKafka with Matchers {
@@ -72,7 +72,7 @@ class Failover2Spec extends FlatSpec with AffinityTestBase with EmbeddedKafka wi
       }
 
       case HTTP(POST, PATH(key, value), _, response) => handleWith(response) {
-        implicit val timeout = Timeout(1 second)
+        implicit val timeout = Timeout(specTimeout / 5)
         keyspace1 ack PutValue(key, value) map {
           case result => HttpResponse(SeeOther, headers = List(headers.Location(Uri(s"/$key"))))
         }
@@ -100,52 +100,33 @@ class Failover2Spec extends FlatSpec with AffinityTestBase with EmbeddedKafka wi
 
   "Master Transition" should "not lead to inconsistent state" in {
     val requestCount = new AtomicLong(0L)
-    val errorCount = new AtomicLong(0L)
     val expected = new ConcurrentHashMap[String, String]()
     import scala.concurrent.ExecutionContext.Implicits.global
 
-    val client = new Thread {
-
-      override def run: Unit = {
-        val random = new Random()
-        val requests = scala.collection.mutable.ListBuffer[Future[String]]()
-        for (i <- (1 to 250)) {
-          if (isInterrupted) throw new InterruptedException
-          val key = random.nextInt.toString
-          val value = random.nextInt.toString
-          requests += node1.http(POST, s"/$key/$value") map {
-            case response =>
-              if (i == 100) {
-                //after a few writes have succeeded kill one node
-                node2.shutdown()
-              }
-              expected.put(key, value)
-              response.status.value
-          } recover {
-            case e: Throwable => e.getMessage
+    val random = new Random()
+    val requests = scala.collection.mutable.ListBuffer[Future[Try[StatusCode]]]()
+    for (i <- (1 to 250)) {
+      val key = random.nextInt.toString
+      val value = random.nextInt.toString
+      requests += node1.http(POST, s"/$key/$value") map {
+        case response =>
+          if (i == 100) {
+            //after a few writes have succeeded kill one node
+            node2.shutdown()
           }
-        }
-        requestCount.set(requests.size)
-        try {
-          val statuses = Await.result(Future.sequence(requests), specTimeout).groupBy(x => x).map {
-            case (status, list) => (status, list.length)
-          }
-          errorCount.set(requestCount.get - statuses("303 See Other"))
-        } catch {
-          case e: Throwable =>
-            e.printStackTrace()
-            errorCount.set(requests.size)
-        }
-
+          expected.put(key, value)
+          Success(response.status)
+      } recover {
+        case e: Throwable => Failure(e)
       }
     }
-    client.start
-    client.join(specTimeout.toMillis)
-    errorCount.get should be(0L)
-    expected.asScala.foreach{ case (key, value) =>
+    requestCount.set(requests.size)
+    Await.result(Future.sequence(requests), specTimeout).foreach(_ should be(Success(SeeOther)))
+
+    expected.asScala.foreach { case (key, value) =>
       val y = Await.result(node1.http(GET, s"/$key").map { response => response.entity }, specTimeout / 3)
       val x = jsonStringEntity(value)
-      y should be (x)
+      y should be(x)
     }
   }
 
